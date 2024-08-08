197a7e2b21146395ce77b6c2c5fab0063d991a0a
kavin <kavinvelavan122@gmail.com>
2024-08-08 09:27:40 +0530

    updated assessment service

--- AssessmentApi/src/main/java/com/ust/AssessmentApi/repository/Optionsrepository.java
+++
@@ -1,8 +0,0 @@
-package com.ust.AssessmentApi.repository;
-
-import com.ust.AssessmentApi.model.Options;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Optionsrepository extends JpaRepository<Options,Long> {
-
-}
--- AssessmentApi/src/main/java/com/ust/AssessmentApi/repository/Setrepository.java
+++
@@ -1,8 +0,0 @@
-package com.ust.AssessmentApi.repository;
-
-import com.ust.AssessmentApi.model.Set;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Setrepository extends JpaRepository<Set,Long> {
-    Set findBySetname(String setname);
-}
