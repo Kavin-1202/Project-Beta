da22e9630b4123a389c64f22a528f3542d86561a
kavin <kavinvelavan122@gmail.com>
2024-08-12 09:44:43 +0530

    todo updated

---
+++ todoapp/src/TodoList.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import TodoItem from './TodoItem';
+import './TodoList.css';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTodo = () => {
+    if (inputValue.trim() === '') return;
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setInputValue('');
+  };
+
+  const toggleComplete = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const removeTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <div className="todos">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
